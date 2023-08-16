96940cea1f46d21f443feca67d32806b1c44bc02
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-08-16 21:55:53 +0530

    code update

---
+++ p5 .js
@@ -0,0 +1,4 @@
+/*Console log the result of adding two numbers, 5 and 10, using the + operator.
+Note: This task should be completed on a single line of code. The output in the console should be 15.*/
+
+console.log(10 + 5);
---
+++ p6.js
@@ -0,0 +1,14 @@
+/*Code three variables:
+
+The first variable should be a string with the following value: "Now in ". Name the variable now.
+
+The second variable should be a number with the value: 3. Name the variable three.
+
+The third variable should a string with the following value: "D!". Name the variable d.
+
+Console log the following code: now + three + d.
+
+Note: The expected output should be: "Now in 3D!".*/
+
+    var now = "Now in ", three = 3, d = 'D!'; 
+    console.log(now + three + d);
